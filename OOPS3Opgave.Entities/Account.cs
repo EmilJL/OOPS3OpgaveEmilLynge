2366503483e0e585d825cc4752aed9b9a7135654
Emil van der Wehle Jörn Lynge <emil5923@edu.campusvejle.dk>
2018-08-23 13:38:57 +0200

    Hmm

---
+++ OOPS3Opgave.Entities/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS3Opgave.Entities
+{
+    public abstract class Account
+    {
+        private decimal Balance;
+        private bool myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        public decimal MyProperty
+        {
+            get { return Balance; }
+        }
+
+    }
+}
---
+++ OOPS3Opgave.Entities/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS3Opgave.Entities
+{
+    public abstract class Customer
+    {
+        private decimal transactionCost;
+        private decimal monthlyAccountFee;
+        private string cpr;
+        private string name;
+        
+        public Customer(string name, string cpr, Account account)
+        {
+            Name = name;
+            CPR = cpr;
+            MonthlyAccountFee = CalculateCostOfMonth(, )
+        }
+
+        public decimal CalculateCostOfMonth(Month month, decimal monthlyAccountFee, decimal transactionCost)
+        {
+            return 0;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public string CPR
+        {
+            get { return cpr; }
+            set { cpr = value; }
+        }
+
+
+        public decimal MonthlyAccountFee
+        {
+            get { return monthlyAccountFee; }
+            set { monthlyAccountFee = value; }
+        }
+
+
+        public decimal TransactionCost
+        {
+            get { return transactionCost; }
+            set { transactionCost = value; }
+        }
+
+    }
+}
